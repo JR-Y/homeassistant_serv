c5b6e9ec2c8210b664d55622b166bc84b3700551
JR-Y <robert.yrjola@gmail.com>
2021-10-31 11:32:42 +0200

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button onClick={async () => {
+        let headers = new Headers();
+        headers.append("Content-Type", "application/json");
+        const params = {
+          method: 'POST',
+          headers: headers,
+          body: JSON.stringify({ "entity_id": "switch.ikea_outlet_1_sanna_autopistoke" })
+        };
+        //@ts-ignore
+        const request = new Request(`http://192.168.35.8:9467/api/ha/services/switch/turn_on`, params);
+        const response = await fetch(request);
+        console.log(response.body)
+
+      }}>TURN ON</button>
+      <button onClick={async () => {
+        let headers = new Headers();
+        headers.append("Content-Type", "application/json");
+        const params = {
+          method: 'POST',
+          headers: headers,
+          body: JSON.stringify({ "entity_id": "switch.ikea_outlet_1_sanna_autopistoke" })
+        };
+        //@ts-ignore
+        const request = new Request(`http://192.168.35.8:9467/api/ha/services/switch/turn_off`, params);
+        const response = await fetch(request);
+        console.log(response.body)
+
+      }}>TURN OFF</button>
+      <input type="time" onChange={(ev) => {
+        console.log(ev.target.value)
+      }}></input>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,230 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const bodyParser = require('body-parser')
+const path = require('path');
+const axios = require('axios').default;
+const WebSocket = require("ws");
+
+const port = process.env.PORT;
+const HA_ROOT_URL = `http://${process.env.HA_HOST}:${process.env.HA_PORT}`;
+const HA_TOKEN = process.env.HA_TOKEN;
+let HA_SENSOR_OUTDOOR_TEMPERATURE = process.env.HA_SENSOR_OUTDOOR_TEMPERATURE;
+let HA_SENSOR_INDOOR_TEMPERATURE = process.env.HA_SENSOR_INDOOR_TEMPERATURE;
+
+const ws = new WebSocket(`ws://${process.env.HA_HOST}:${process.env.HA_PORT}/api/websocket`);
+
+
+
+const SWITCH_ACTIONS = ["turn_on", "turn_off"]
+
+//HA message types
+const AUTH_REQUIRED = "auth_required";
+const AUTH_OK = "auth_ok";
+const RESULT = "result";
+const EVENT = "event";
+
+//APP state
+let message_id = 0;
+let auth_ok;
+let resultQueue = {};
+
+//Unique messageID to be returned with homeassistant result messages
+function getMessageId() {
+    message_id++;
+    return message_id;
+}
+// ws.on("open", (sock) => {
+//     console.log("open")
+// })
+
+ws.on("message", message => {
+    try {
+        let json = JSON.parse(message);
+        if (json.type && json.type) {
+            //console.log(JSON.stringify(json))
+            switch (json.type) {
+                case AUTH_REQUIRED:
+                    ws.send(JSON.stringify({
+                        "type": "auth",
+                        "access_token": HA_TOKEN
+                    }))
+                    break;
+                case AUTH_OK:
+                    auth_ok = true;
+                    authenticated();
+                    break;
+                case RESULT:
+                    console.log(resultQueue[json.id])
+                    console.log(json)
+                    if (resultQueue[json.id] && json.success) {
+                        console.log("success")
+                        delete resultQueue[json.id];
+                    }
+                    break;
+                case EVENT:
+
+                    break;
+                default:
+                    break;
+            }
+        }
+    } catch (error) {
+        console.log(error)
+    }
+})
+function authenticated() {
+    subscribeEvents()
+}
+function subscribeEvents() {
+    sendHaMessage({
+        "type": "subscribe_events",
+        // Optional
+        "event_type": "state_changed"
+    })
+}
+
+function turnOnLight(entityId) {
+    if (auth_ok) {
+        sendHaMessage({
+            "type": "call_service",
+            "domain": "light",
+            "service": "turn_off",
+            "service_data": {
+                "entity_id": entityId
+            }
+        })
+    }
+}
+function operateSwitch(service, entityId) {
+    if (auth_ok) {
+        sendHaMessage({
+            "type": "call_service",
+            "domain": "switch",
+            "service": service,
+            "service_data": {
+                "entity_id": entityId
+            }
+        })
+    }
+}
+
+let OUTDOOR_TEMPERATURE;
+
+app.use(express.static(path.join(__dirname, '..', 'client/build')))
+app.use(bodyParser.json())
+
+app.get('/api/temperature/outdoor', (req, res) => {
+    axios.get(`${HA_ROOT_URL}/api/states/${HA_SENSOR_OUTDOOR_TEMPERATURE}`,
+        {
+            headers: {
+                "Authorization": `Bearer ${HA_TOKEN}`
+            }
+        }
+    ).then(result => {
+        if (result.data && result.data.state) {
+            try {
+                OUTDOOR_TEMPERATURE = Number.parseFloat(result.data.state);
+            } catch (error) {
+
+            }
+        }
+        res.send(`${OUTDOOR_TEMPERATURE}`)
+    }).catch(err => {
+        console.log(err)
+        res.status(400).send()
+    })
+})
+app.get('/api/temperature/indoor', (req, res) => {
+    axios.get(`${HA_ROOT_URL}/api/states/${HA_SENSOR_INDOOR_TEMPERATURE}`,
+        {
+            headers: {
+                "Authorization": `Bearer ${HA_TOKEN}`
+            }
+        }
+    ).then(result => {
+        if (result.data && result.data.state) {
+            try {
+                OUTDOOR_TEMPERATURE = Number.parseFloat(result.data.state);
+            } catch (error) {
+
+            }
+        }
+        res.send(`${OUTDOOR_TEMPERATURE}`)
+    }).catch(err => {
+        console.log(err)
+        res.status(400).send()
+    })
+})
+
+function sendHaMessage(object) {
+    let id = getMessageId();
+    resultQueue[id] = {
+        timeStamp: new Date(),
+        message: {
+            "id": id,
+            ...object
+        }
+    }
+    ws.send(JSON.stringify(resultQueue[id].message))
+}
+
+app.get('/api/ha/states/', (req, res) => {
+    sendHaMessage({ "type": "get_states" });
+    axios.get(`${HA_ROOT_URL}/api/states`,
+        {
+            headers: {
+                "Authorization": `Bearer ${HA_TOKEN}`
+            }
+        }
+    ).then(result => {
+        res.json(result.data)
+    }).catch(err => {
+        console.log(err)
+        res.status(400).send()
+    })
+})
+
+app.get('/api/ha/services/', (req, res) => {
+    axios.get(`${HA_ROOT_URL}/api/services`,
+        {
+            headers: {
+                "Authorization": `Bearer ${HA_TOKEN}`
+            }
+        }
+    ).then(result => {
+        res.json(result.data)
+    }).catch(err => {
+        console.log(err)
+        res.status(400).send()
+    })
+})
+
+app.get('/api/ha/events/', (req, res) => {
+    axios.get(`${HA_ROOT_URL}/api/events`,
+        {
+            headers: {
+                "Authorization": `Bearer ${HA_TOKEN}`
+            }
+        }
+    ).then(result => {
+        res.json(result.data)
+    }).catch(err => {
+        console.log(err)
+        res.status(400).send()
+    })
+})
+
+app.post(`/api/ha/services/switch/:action`, (req, res) => {
+    const action = req.params.action;
+    const body = req.body;
+    operateSwitch(action, body.entity_id)
+})
+
+app.listen(port, () => {
+    console.log(`homeassist_srv listening at http://localhost:${port}`)
+})
